3fd4988eb07e2d79d672edfcf6c132ad37a8c2f3
50Start <74140514+50start@users.noreply.github.com>
2021-08-16 00:31:15 +0900

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,7 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDrknDyFgGmfhVOwPVyhvCFU__fQ7bu58",
-  authDomain: "fir-storage2-b4e01.firebaseapp.com",
-  projectId: "fir-storage2-b4e01",
-  storageBucket: "fir-storage2-b4e01.appspot.com",
-  messagingSenderId: "836727656999",
-  appId: "1:836727656999:web:c5f4aa3367939d2352f402",
+  
 };
 
 firebase.initializeApp(firebaseConfig);
